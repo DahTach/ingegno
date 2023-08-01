185568b03a8d3db07281e38608ab2c27db6e208d
Francesco Tacinelli <francesco.tacinelli@csgalileo.net>
2023-08-01 11:33:35 +0200

    landing page with theme toggle and angular-three example

--- frontend/web-ingegno/src/app/app.component.ts
+++ frontend/web-ingegno/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, Renderer2 } from "@angular/core";
+import { ThemeService } from "./services/theme.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "web-ingegno";
+
+  constructor(
+    private themeService: ThemeService,
+    private renderer: Renderer2,
+  ) {}
+
+  ngOnInit(): void {
+    this.themeService.themeChanges().subscribe((theme) => {
+      if (theme.oldValue) {
+        this.renderer.setAttribute(
+          document.body,
+          "data-bs-theme",
+          theme.oldValue,
+        );
+      }
+      this.renderer.setAttribute(
+        document.body,
+        "data-bs-theme",
+        theme.newValue,
+      );
+    });
+  }
 }
--- frontend/web-ingegno/src/app/app.module.ts
+++ frontend/web-ingegno/src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ThreeTestingComponent } from "./components/three-testing/three-testing.component";
+import { Scene } from "./components/three-scene/three-scene.component";
+import { AccordionTestComponent } from "./components/accordion-test/accordion-test.component";
+import { PageNavigationComponent } from "./components/page-navigation/page-navigation.component";
+import { ThemeToggleComponent } from "./components/theme-toggle/theme-toggle.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, LandingPageComponent],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    ThreeTestingComponent,
+    Scene,
+    AccordionTestComponent,
+    PageNavigationComponent,
+    ThemeToggleComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
