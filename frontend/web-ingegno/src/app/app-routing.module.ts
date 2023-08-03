5fffd2cb86d409396aebb00b9f276947f09edcb2
ngiacobbo <nicola.giacobbo@csgalileo.org>
2023-08-03 08:24:16 +0200

    feat(pages): first few pages

---
+++ frontend/web-ingegno/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ThreeComponent } from './pages/three/three.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'three', component: ThreeComponent },
+    { path: '**', component: PageNotFoundComponent }
+]; // sets up routes constant where you define your routes
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- frontend/web-ingegno/src/app/app.module.ts
+++ frontend/web-ingegno/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ThreeComponent } from './pages/three/three.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent, ThreeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
