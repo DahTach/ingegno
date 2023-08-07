63018b761c4d5e26ec64a99ba8a92320989c7018
ngiacobbo <nicola.giacobbo@csgalileo.org>
2023-08-07 14:51:56 +0200

    feat: added room component

--- frontend/web-ingegno/src/app/app-routing.module.ts
+++ frontend/web-ingegno/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { ThreeComponent } from './pages/three/three.component';
+import { RoomComponent } from './components/room/room.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'three', component: ThreeComponent },
+    { path: 'room', component: RoomComponent },
     { path: '**', component: PageNotFoundComponent }
 ]; // sets up routes constant where you define your routes
 
