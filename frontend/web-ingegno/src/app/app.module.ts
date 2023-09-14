74907f2315b1c85fd4e453fc9f1e25b2da0c3e74
ngiacobbo <nicola.giacobbo@csgalileo.org>
2023-09-14 15:49:30 +0200

    chore(firebase): add firebase tools

--- frontend/web-ingegno/src/app/app.module.ts
+++ frontend/web-ingegno/src/app/app.module.ts
@@ -1,35 +1,46 @@
 import { NgModule } from "@angular/core";
+import { environment } from "src/environments/environment";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { HomeComponent } from './pages/home/home.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { CanvasBoxComponent } from './components/logo-box/logo-box.component';
-import { RoomComponent } from './components/room/room.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TeamComponent } from './pages/team/team.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from "./pages/home/home.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+import { CanvasBoxComponent } from "./components/logo-box/logo-box.component";
+import { RoomComponent } from "./components/room/room.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { MdbAccordionModule } from "mdb-angular-ui-kit/accordion";
+import { MdbCarouselModule } from "mdb-angular-ui-kit/carousel";
+import { MdbCheckboxModule } from "mdb-angular-ui-kit/checkbox";
+import { MdbCollapseModule } from "mdb-angular-ui-kit/collapse";
+import { MdbDropdownModule } from "mdb-angular-ui-kit/dropdown";
+import { MdbFormsModule } from "mdb-angular-ui-kit/forms";
+import { MdbModalModule } from "mdb-angular-ui-kit/modal";
+import { MdbPopoverModule } from "mdb-angular-ui-kit/popover";
+import { MdbRadioModule } from "mdb-angular-ui-kit/radio";
+import { MdbRangeModule } from "mdb-angular-ui-kit/range";
+import { MdbRippleModule } from "mdb-angular-ui-kit/ripple";
+import { MdbScrollspyModule } from "mdb-angular-ui-kit/scrollspy";
+import { MdbTabsModule } from "mdb-angular-ui-kit/tabs";
+import { MdbTooltipModule } from "mdb-angular-ui-kit/tooltip";
+import { MdbValidationModule } from "mdb-angular-ui-kit/validation";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TeamComponent } from "./pages/team/team.component";
+import { ProfileComponent } from "./components/profile/profile.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PageNotFoundComponent, CanvasBoxComponent, RoomComponent, HeaderComponent, FooterComponent, TeamComponent, ProfileComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    CanvasBoxComponent,
+    RoomComponent,
+    HeaderComponent,
+    FooterComponent,
+    TeamComponent,
+    ProfileComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -48,8 +59,8 @@ import { ProfileComponent } from './components/profile/profile.component';
     MdbTabsModule,
     MdbTooltipModule,
     MdbValidationModule,
-    BrowserAnimationsModule
-    ],
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
